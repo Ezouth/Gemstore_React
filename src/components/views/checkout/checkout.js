6b25f60e7e24303a0fe03b819132c8b91b254443
Tyler Whidden <Ty.Whidden@gmail.com>
2018-10-03 11:49:04 -0400

    View structure created. TODO: View functionality.

---
+++ src/components/views/checkout/checkout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Checkout from './checkout.css';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ src/components/views/home/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from './home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+      </div>
+    );
+  }
+}
+
+export default Home;
