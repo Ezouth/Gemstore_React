ccb24ef991a5a743c7b7bba05a489f2a794c3b8f
Tyler Whidden <Ty.Whidden@gmail.com>
2018-10-03 14:28:00 -0400

    Weird error

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+
 ReactDOM.render(
   <BrowserRouter>
   <App />
   </BrowserRouter>, document.getElementById('root'));
 
+// hot reloads pages to keep state (not refreshing it)
 
-
-  // hot reloads pages to keep state (not refreshing it)
 if (module.hot) {
   module.hot.accept();
 }
